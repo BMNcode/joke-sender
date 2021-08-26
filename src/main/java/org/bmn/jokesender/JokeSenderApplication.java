31267494b199d38e14d6fb84c833b3fc93d88aa3
Maksim Bulenkov <bulenkovmaksim89@gmail.com>
2021-08-26 14:43:25 +0300

    init

---
+++ src/main/java/org/bmn/jokesender/JokeSenderApplication.java
@@ -0,0 +1,13 @@
+package org.bmn.jokesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bmn/jokesender/JokeSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bmn.jokesender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
